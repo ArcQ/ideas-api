31448621b5237d5c96cbabfcd99e9c1deecf5101
Alanna Tai <alanna.tai@hotmail.com>
2021-03-21 01:57:56 -0400

    Add notifications, chat actions, idea details screen to main app stack

--- mobile/src/navigation/MainStackNavigator.jsx
+++ mobile/src/navigation/MainStackNavigator.jsx
@@ -1,10 +1,20 @@
 import { createNativeStackNavigator } from 'react-native-screens/native-stack';
 import React from 'react';
 
-import { CHAT_ROUTE, CREATE_IDEA_ROUTE, MAIN_IDEAS_LIST_ROUTE } from '../constants/routes';
+import { 
+  CHAT_ROUTE,
+  CREATE_IDEA_ROUTE,
+  MAIN_IDEAS_LIST_ROUTE,
+  NOTIFICATIONS_ROUTE,
+  CHAT_ACTIONS_ROUTE,
+  IDEA_DETAIL_ROUTE,
+} from '../constants/routes';
 import CreateIdeaContainer from '../screens/CreateIdeaScreen/CreateIdeaContainer';
 import ChatContainer from '../screens/ChatScreen/ChatContainer';
 import IdeasListContainer from '../screens/IdeasListScreen/IdeasListContainer';
+import Notifications from '../screens/NotificationsScreen';
+import IdeaDetailContainer from '../screens/IdeaDetailScreen/IdeaDetailContainer';
+import ChatActions from '../screens/ChatActionsScreen/ChatActions';
 
 const Stack = createNativeStackNavigator();
 
@@ -49,6 +59,23 @@ export default function MainStackNavigator() {
         options={{ 
           headerShown: false
         }}
+      />
+      <Stack.Screen
+        name={NOTIFICATIONS_ROUTE}
+        title="Notifications"
+        component={Notifications}
+      />
+      <Stack.Screen
+        name={CHAT_ACTIONS_ROUTE}
+        title="ChatActions"
+        component={ChatActions}
+        options={{ stackPresentation: 'modal' }}
+      />
+      <Stack.Screen
+        name={IDEA_DETAIL_ROUTE}
+        title="Idea Detail"
+        component={IdeaDetailContainer}
+        options={{ headerShown: false }}
       />
     </Stack.Navigator>
   );
